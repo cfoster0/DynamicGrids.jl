ba37577c0572321f7c5c9bfcf7b1c2464c37c086
Charles Foster <cfoster0@stanford.edu>
2021-03-08 17:13:10 -0800

    Rename sd to simdata in step! definition
    Should fix bug in direct calls to step!

--- src/framework.jl
+++ src/framework.jl
@@ -207,8 +207,8 @@ simdata[:a]
 
 This example returns a `GridData` object for the `:a` grid, which is `<: AbstractAray`.
 """
-function step!(sd::SimData)
-    _updatetime(sd, currentframe(sd) + 1) |> _proc_setup |> sd -> _step!(sd, rules(simdata))
+function step!(simdata::SimData)
+    _updatetime(simdata, currentframe(simdata) + 1) |> _proc_setup |> simdata -> _step!(simdata, rules(simdata))
 end
 
 # Allows different processors to modify the simdata object
